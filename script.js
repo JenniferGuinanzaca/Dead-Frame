d0995bffcd132aaf07b8b3752fcfdf82726f71e9
Jennifer <Jennyflox12@gmail.com>
2026-09-15 15:05:27 -0400

    style footer

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // Dead Frame JS
+
+const movies=[
+    {
+        title: "Scream",
+        year: 1996,
+        director: "Wes Craven",
+        genre: "Slasher",
+        description: "A masked killer terrorizes a small town, targeting teenagers.",
+    },
+
+    {
+        
+    }
+]
