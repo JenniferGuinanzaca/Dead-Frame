d90010af0bb72ba98cb47c43eb5b0ed99c40a38c
Jennifer <Jennyflox12@gmail.com>
2026-09-15 15:19:56 -0400

    add movie data to array

---
+++ js/movies.js
@@ -0,0 +1,107 @@
+// Dead Frame JS
+
+const movies=[
+    {
+        title: "Scream",
+        year: 1996,
+        director: "Wes Craven",
+        genre: "Slasher",
+        description: "A masked killer terrorizes a small town, targeting teenagers.",
+    },
+
+    {
+       title: "Midsommar",
+       year: 2019,
+       director: "Ari Aster",
+       genre: "psychological",
+       description: "A group of friends travel to Sweden for a festival that turns sinister.",
+    }
+
+    {
+        title: "The Shining",
+        year: 1980,
+        director: "Stanley Kubrick",
+        genre: "psychological",
+        description: "A family becomes isolated in a haunted hotel, leading to madness.",
+    }
+
+    {
+        title: "Get Out",
+        year: 2017,
+        director: "Jordan Peele",
+        genre: "psychological",
+        description: "A young African-American man visits his white girlfriend's family estate, uncovering disturbing secrets.",
+    }
+
+    {
+        title: "The Conjuring",
+        year: 2013,
+        director: "James Wan",
+        genre: "supernatural",
+        description: "Paranormal investigators help a family terrorized by a dark presence in their farmhouse.",
+    }
+
+    {
+        title: "Hereditary",
+        year: 2018,
+        director: "Ari Aster",
+        genre: "psychological",
+        description: "A grieving family is haunted by tragic and disturbing occurrences.",
+    }
+
+    {
+        title: "The Exorcist",
+        year: 1973,
+        director: "William Friedkin",
+        genre: "supernatural",
+        description: "A young girl is possessed by a demonic entity, leading to a terrifying exorcism.",
+    }
+
+    {
+        title: "A Nightmare on Elm Street",
+        year: 1984,
+        director: "Wes Craven",
+        genre: "slasher",
+        description: "A group of teenagers are stalked and killed in their dreams by the vengeful Freddy Krueger.",
+    }
+
+    {
+        title: "Halloween",
+        year: 1978,
+        director: "John Carpenter",
+        genre: "slasher",
+        description: "A masked killer escapes from a mental institution and terrorizes a small town on Halloween night.",
+    }
+
+    {
+        title: "Insidious",
+        year: 2010,
+        director: "James Wan",
+        genre: "supernatural",
+        description: "A family tries to prevent evil spirits from trapping their comatose child in a realm called The Further.",
+    }
+
+    {
+        title: "28 Days Later",
+        year: 2002,
+        director: "Danny Boyle",
+        genre: "zombie",
+        description: "A virus outbreak causes a zombie apocalypse in the UK, and survivors struggle to stay alive.",
+    }
+
+    {
+        title: "Train to Busan",
+        year: 2016,
+        director: "Yeon Sang-ho",
+        genre: "zombie",
+        description: "Passengers on a train to Busan must fight for survival during a zombie outbreak.",
+    }
+
+    {
+        title: "Night of the Living Dead",
+        year: 1968,
+        director: "George A. Romero",
+        genre: "zombie",
+        description: "A group of people barricade themselves in a farmhouse to survive a zombie invasion.",
+    }
+]
